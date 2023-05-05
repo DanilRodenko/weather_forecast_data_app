be8072d4b9ace72dd7f6020a591cad450d78fa73
DR <94643493+DanilRodenko@users.noreply.github.com>
2023-05-05 15:07:33 +0100

    Add images in sky conditions

--- main.py
+++ main.py
@@ -10,12 +10,27 @@ option = st.selectbox("Selece data to view",
                       ("Temperatures", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
-d, t = get_data(days)
 
-figure = px.line(x=d, y=t,
-                 labels={"x": "Date", "y": "Temperatures"})
-st.plotly_chart(figure)
+if place:
+    try:
+        filtered_data = get_data(place, days)
+
+        if option == 'Temperatures':
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperatures"})
+            st.plotly_chart(figure)
+
+        if option == 'Sky':
+            images = {"Clear" : "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain" : "images/rain.png", "Snow" : "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+
+            st.image(image_paths, width=115)
+    except KeyError:
+        st.subheader("City is not found")
+
 
 
 
